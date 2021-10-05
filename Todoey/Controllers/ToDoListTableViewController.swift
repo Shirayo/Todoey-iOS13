e12d3bf93c429ad8be3c98d92c8d1152f4da3e1c
Vasily <iphoje@gmail.com>
2021-10-05 15:39:37 +0300

    added Item and Category classes for Realm

---
+++ Todoey/Controllers/ToDoListTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ToDoListTableViewController.swift
+//  Todoey
+//
+//  Created by Shirayo on 05.10.2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+
+}
